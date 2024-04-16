f741ee6789c005a76f62d74b8233fa5c5d5d5b51
Mari <mariannasantiagogodoy58@gmail.com>
2024-04-16 11:52:21 -0300

    Mari

---
+++ Marisg/Results.cs
@@ -0,0 +1,32 @@
+namespace Marisg;
+
+public class
+{
+    public int temp{get; set;}
+
+    public string description{get; set;}
+
+    public string city{get;set;}
+
+    public double rain{get;set;}
+
+    public int himidity{get;set;}
+
+    public string sunrise{get;set;}
+
+    public string sunset{get;set;}
+
+    public string wind_speed{get;set;}
+
+    public string wind_direction{get;set;}
+
+    public string moon_phase{get;set;}
+
+    public string currently{get;set;}
+
+    public int img_id{get;set;}
+
+    public string sunrise{get;set;}
+
+    public string codition_code{get;set;}
+}
